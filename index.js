b175c4c909cbded27f1b219a723134030092db51
fatisenko <fatihsenko@gmail.com>
2022-03-02 01:13:28 +0300

    some changes

--- index.js
+++ index.js
@@ -6,19 +6,10 @@ const pElement = document.getElementById("reverseClause");
 btn.addEventListener("click", (e) =>{
     e.preventDefault();
      if(!inputArea.value){
-        pElement.innerHTML = "Please Enter A Sentence";
-        setInterval(()=>{pElement.innerHTML = ""},3000);
-        
+        pElement.innerHTML = "Please Enter A Sentence"; 
      }
      else{
-        pElement.innerHTML = inputArea.value.split(" ").reverse().join(" ");
-        
+        pElement.innerHTML = inputArea.value.split(" ").reverse().join(" ");    
 }
 })
 
-inputArea.addEventListener("click", (e) =>{
-    e.preventDefault();
-    pElement.innerHTML = "";
-    inputArea.value = "";
-    });
-      
