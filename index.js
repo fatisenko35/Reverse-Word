569badf49fc8908a45f4a2d3a6d3a0f8e7b1d782
fatisenko <fatihsenko@gmail.com>
2022-03-02 01:08:56 +0300

    done

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const btn = document.getElementById("turnerButton");
 const pElement = document.getElementById("reverseClause");
 
 
-btn.addEventListener("click", () =>{
-    pElement.innerHTML = inputArea.value.split(" ").reverse().join(" ");
-    inputArea.value = "";
-    inputArea.addEventListener("click", () =>{
-        pElement.innerHTML = "";
-    })
+btn.addEventListener("click", (e) =>{
+    e.preventDefault();
+     if(!inputArea.value){
+        pElement.innerHTML = "Please Enter A Sentence";
+        setInterval(()=>{pElement.innerHTML = ""},3000);
+        
+     }
+     else{
+        pElement.innerHTML = inputArea.value.split(" ").reverse().join(" ");
+        
+}
 })
+
+inputArea.addEventListener("click", (e) =>{
+    e.preventDefault();
+    pElement.innerHTML = "";
+    inputArea.value = "";
+    });
+      
